a19e34762c971d0d61b7fd4eb99f30d873e87a63
Adam E <adamelsayad2002@gmail.com>
2025-01-20 03:38:54 -0500

    Inital Commit

---
+++ react-web-template/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Appbar from './components/Appbar'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className= "App">
+      <Appbar />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
